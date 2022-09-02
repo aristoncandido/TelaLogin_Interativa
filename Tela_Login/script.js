da09f50fdf671236807616a5cb789ca493c8d504
Ariston <aristoncandido.nw@gmail.com>
2022-09-02 18:57:16 -0300

    adicionei o js

--- Tela_Login/script.js
+++ Tela_Login/script.js
@@ -3,12 +3,33 @@
 function baratinar()
 {
 
-    const tel = document.getElementById("tela_login")
+    const tel = document.getElementById("tela");
+
+    
+    
+    const x = tel.style.transition ="1.5s";
+    const y = tel.style.right ="200px";
 
 
 
+    
+ 
 
-    tel =
+
+
+}
+
+function baratinar2()
+{
+
+    const tel = document.getElementById("tela");
+
+    const y = tel.style.transition ="1.5s";
+    
+
+    const x = tel.style.right = " 0px"; 
+
+
 
 
 
